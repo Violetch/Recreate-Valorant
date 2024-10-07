62083a6fc55fc5beb0cceca553dedbcc45d224ae
harantsuki <anyazosen@gmail.com>
2024-10-08 03:50:47 +0700

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,10 @@ const router = createHashRouter([
     path: "/",
     element: <Landing />,
   },
+  {
+    path: "/Remake-Valorant",
+    element: <Landing />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
